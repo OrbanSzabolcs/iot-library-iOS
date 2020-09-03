376d6e67e11b00a40b0b5fb7d3616b395ff069c4
Orban Szabolcs <szabolcs.orban@zynksoftware.com>
2020-09-03 14:41:52 +0300

    Initial project setup

---
+++ iotLibrary/iotLibrary/iotLibrary.h
@@ -0,0 +1,19 @@
+//
+//  iotLibrary.h
+//  iotLibrary
+//
+//  Created by dev on 03/09/2020.
+//  Copyright © 2020 Zynk. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for iotLibrary.
+FOUNDATION_EXPORT double iotLibraryVersionNumber;
+
+//! Project version string for iotLibrary.
+FOUNDATION_EXPORT const unsigned char iotLibraryVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <iotLibrary/PublicHeader.h>
+
+
---
+++ iotLibrary/iotLibrarySample/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  iotLibrarySample
+//
+//  Created by dev on 03/09/2020.
+//  Copyright © 2020 Zynk. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
